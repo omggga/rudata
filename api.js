39c25271238085939ab19ad361f9bc0e1cbb197a
omggga <omggga@gmail.com>
2023-07-26 20:58:03 +0300

    Added a lot of new methods for Rudata API, now accept batch query

--- api.js
+++ api.js
@@ -1,35 +1,48 @@
 'use strict'
 
-const utils = require('./utils')
-const config = require('./config')
+const utils = require('utils')
+const config = require('./app.config')
+require('./all-settled')
 
-//Default routing config
 const route = {
 	getEndOfDayOnExchanges: {
 		url: 'Archive/EndOfDayOnExchanges/',
 		defaults: {
-			official: false
-		}
+			official: false,
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
 	},
 	getInstruments: {
 		url: 'Info/Instruments/',
 		defaults: {
 			pageNum: 1,
-			pageSize: 1000
+			pageSize: 300
 		},
 		havePages: true
 	},
 	getSecurities: {
 		url: 'Info/Securities/',
 		defaults: {
-			count: 20000
-		}
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
+	},
+	getCalendarV2: {
+		url: 'Info/CalendarV2/',
+		defaults: {
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
 	},
 	getConstituents: {
 		url: 'Indicator/Constituents/',
 		defaults: {
 			pageNum: 1,
-			pageSize: 1000
+			pageSize: 300
 		},
 		havePages: true
 	},
@@ -48,11 +61,63 @@ const route = {
 	getClassification: {
 		url: 'Bond/Classification/',
 		defaults: {
-			count: 50000
-		}
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
+	},
+	getIndicatorList: {
+		url: 'Indicator/List',
+		defaults: {
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
 	},
 	getDateOptions: {
 		url: 'Bond/DateOptions/'
+	},
+	getTimeTable: {
+		url: 'Bond/TimeTable/',
+		defaults: {
+			pageNum: 1,
+			pageSize: 300
+		},
+		havePages: true
+	},
+	getCouponsExt: {
+		url: 'Bond/CouponsExt/',
+		defaults: {
+			pageNum: 1,
+			pageSize: 300
+		},
+		havePages: true
+	},
+	getBondCoupons: {
+		url: 'Bond/',
+		defaults: {
+			pageNum: 1,
+			pageSize: 300
+		},
+		havePages: true,
+		prependID: 'id',
+		prependUrl: '/Coupons'
+	},
+	getBonds: {
+		url: 'Bond/Coupons/',
+		defaults: {
+			pageNum: 1,
+			pageSize: 300
+		},
+		havePages: true
+	},
+	getBondOffers: {
+		url: 'Bond/Offers/',
+		defaults: {
+			pageNum: 1,
+			pageSize: 300
+		},
+		havePages: true
 	},
 	getCompanyRatingsHist: {
 		url: 'Rating/CompanyRatingsHist/',
@@ -62,7 +127,7 @@ const route = {
 		url: 'Info/ShareDividend/',
 		defaults: {
 			pageNum: 1,
-			pageSize: 1000
+			pageSize: 300
 		},
 		havePages: true
 	},
@@ -70,7 +135,7 @@ const route = {
 		url: 'Info/ShareDividend/',
 		defaults: {
 			pageNum: 1,
-			pageSize: 1000
+			pageSize: 300
 		},
 		havePages: true,
 		insertID: 'id'
@@ -78,19 +143,66 @@ const route = {
 	getCorporateActions: {
 		url: 'CorporateAction/Actions/',
 		defaults: {
-			count: 20000
-		}
+			pageNum: 1,
+			pageSize: 300
+		},
+		havePages: true
 	},
 	getCurrencyRate: {
 		url: 'Archive/CurrencyRate/'
 	},
 	getEndOfDay: {
 		url: 'Archive/EndOfDay/'
+	},
+	getIFXFintoolRefData: {
+		url: 'Info/IFXFintoolRefData',
+		defaults: {
+			pager: {
+				page: 1,
+				size: 100
+			}
+		},
+		havePages: true
+	},
+	getFintoolReferenceData: {
+		url: 'Info/FintoolReferenceData',
+		defaults: {
+			pager: {
+				page: 1,
+				size: 100
+			}
+		},
+		havePages: true
+	},
+	getBondConvertation: {
+		url: 'Bond/Convertation',
+		defaults: {
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
+	},
+	getFintoolConvertation: {
+		url: 'Info/FintoolConvertation',
+		defaults: {
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
+	},
+	getInfoEmitents: {
+		url: 'Info/Emitents',
+		defaults: {
+			pageNum: 1,
+			pageSize: 100
+		},
+		havePages: true
 	}
 }
 
 class Api {
-	constructor() {
+	constructor(options) {
+		this.limiter = options.limiter
 		this.token = null
 	}
 
@@ -116,102 +228,191 @@ class Api {
 			throw new Error(`Ошибка при получении авторизационного токена, ответ не содержит переменной token`)
 		}
 
-		this.token = 'Bearer ' + result.token
+		this.token = `Bearer ${result.token}`
 	}
 
 	//Итоги торгов по инструментам за период на различных площадках
-	getEndOfDayOnExchanges(filters, packageData) {
-		return this.get(route['getEndOfDayOnExchanges'], filters, packageData)
+	getEndOfDayOnExchanges(filters, packageData, isQuery) {
+		return this.get(route['getEndOfDayOnExchanges'], filters, packageData, isQuery)
 	}
 
 	//Получить краткий справочник по торговым инструментам
-	getInstruments(filters, packageData) {
-		return this.get(route['getInstruments'], filters, packageData)
+	getInstruments(filters, packageData, isQuery) {
+		return this.get(route['getInstruments'], filters, packageData, isQuery)
 	}
 
 	//Итоги торгов по инструментам за период на различных площадках
-	getSecurities(filters, packageData) {
-		return this.get(route['getSecurities'], filters, packageData)
+	getSecurities(filters, packageData, isQuery) {
+		return this.get(route['getSecurities'], filters, packageData, isQuery)
 	}
 
 	//Выборка состава индексов
-	getConstituents(filters, packageData) {
-		return this.get(route['getConstituents'], filters, packageData)
+	getConstituents(filters, packageData, isQuery) {
+		return this.get(route['getConstituents'], filters, packageData, isQuery)
 	}
 
 	//Получить данные о торгах за указанный период
-	getArchiveHistory(filters, packageData) {
-		return this.get(route['getArchiveHistory'], filters, packageData)
+	getArchiveHistory(filters, packageData, isQuery) {
+		return this.get(route['getArchiveHistory'], filters, packageData, isQuery)
 	}
 
 	//Получить рейтинги инструмента, его эмитента и основных организаторов выпуска
-	getSecurityRatingsHist(filters, packageData) {
-		return this.get(route['getSecurityRatingsHist'], filters, packageData)
+	getSecurityRatingsHist(filters, packageData, isQuery) {
+		return this.get(route['getSecurityRatingsHist'], filters, packageData, isQuery)
 	}
 
 	//Значения безрисковых кривых на дату
-	getRiskFee(filters, packageData) {
-		return this.get(route['getRiskFee'], filters, packageData)
+	getRiskFee(filters, packageData, isQuery) {
+		return this.get(route['getRiskFee'], filters, packageData, isQuery)
 	}
 
 	//Получить классификацию выпуска
-	getClassification(filters, packageData) {
-		return this.get(route['getClassification'], filters, packageData)
+	getClassification(filters, packageData, isQuery) {
+		return this.get(route['getClassification'], filters, packageData, isQuery)
+	}
+
+	//Выборка справочника индикаторов
+	getIndicatorList(filters, packageData, isQuery) {
+		return this.get(route['getIndicatorList'], filters, packageData, isQuery)
 	}
 
 	//Получить параметры облигации, зависимые от даты
-	getDateOptions(filters, packageData) {
-		return this.get(route['getDateOptions'], filters, packageData)
+	getDateOptions(filters, packageData, isQuery) {
+		return this.get(route['getDateOptions'], filters, packageData, isQuery)
+	}
+
+	//Получить календарь событий
+	getTimeTable(filters, packageData, isQuery) {
+		return this.get(route['getTimeTable'], filters, packageData, isQuery)
+	}
+	
+	//Получить данные по купонам
+	getBondCoupons(filters, packageData, isQuery) {
+		return this.get(route['getBondCoupons'], filters, packageData, isQuery)
+	}
+
+	//Получить данные по купонам (общее)
+	getBonds(filters, packageData, isQuery) {
+		return this.get(route['getBonds'], filters, packageData, isQuery)
+	}
+
+	//Получить данные по досрочным выкупам/офертам
+	getBondOffers(filters, packageData, isQuery) {
+		return this.get(route['getBondOffers'], filters, packageData, isQuery)
+	}
+
+	//Получить расширенные данные по купонам
+	getCouponsExt(filters, packageData, isQuery) {
+		return this.get(route['getCouponsExt'], filters, packageData, isQuery)
 	}
 
 	//Получить рейтинги компании за период
-	getCompanyRatingsHist(filters, packageData) {
-		return this.get(route['getCompanyRatingsHist'], filters, packageData)
+	getCompanyRatingsHist(filters, packageData, isQuery) {
+		return this.get(route['getCompanyRatingsHist'], filters, packageData, isQuery)
 	}
 
 	//Информация по дивидендам
-	getShareDividend(filters, packageData) {
-		return this.get(route['getShareDividend'], filters, packageData)
+	getShareDividend(filters, packageData, isQuery) {
+		return this.get(route['getShareDividend'], filters, packageData, isQuery)
 	}
 
 	//Информация по дивидендам для конкретной организации
-	getShareDividendById(filters, packageData) {
-		return this.get(route['getShareDividendById'], filters, packageData)
+	getShareDividendById(filters, packageData, isQuery) {
+		return this.get(route['getShareDividendById'], filters, packageData, isQuery)
 	}
 
 	//Получить корпоративные действия
-	getCorporateActions(filters, packageData) {
-		return this.get(route['getCorporateActions'], filters, packageData)
+	getCorporateActions(filters, packageData, isQuery) {
+		return this.get(route['getCorporateActions'], filters, packageData, isQuery)
 	}
 
 	//Получить кросс-курс двух валют
-	getCurrencyRate(filters, packageData) {
-		return this.get(route['getCurrencyRate'], filters, packageData)
+	getCurrencyRate(filters, packageData, isQuery) {
+		return this.get(route['getCurrencyRate'], filters, packageData, isQuery)
 	}
 
 	//Получить данные по результатам торгов на заданную дату
-	getEndOfDay(filters, packageData) {
-		return this.get(route['getEndOfDay'], filters, packageData)
+	getEndOfDay(filters, packageData, isQuery) {
+		return this.get(route['getEndOfDay'], filters, packageData, isQuery)
 	}
 
-	async get(optionsConfig, filters, packageData) {
-		if (!packageData) {
-			const data = await this.getData(optionsConfig, filters)
-			return utils.obj2xml({ 'item-list': { item: data } })
+	//Получить справочник Интерфакс по финансовым инструментам
+	getIFXFintoolRefData(filters, packageData, isQuery) {
+		return this.get(route['getIFXFintoolRefData'], filters, packageData, isQuery)
+	}
+
+	//Получить данные по конвертациям облигаций в другие инструменты
+	getBondConvertation(filters, packageData, isQuery) {
+		return this.get(route['getBondConvertation'], filters, packageData, isQuery)
+	}
+
+	//Получить данные по конвертациям инструментов в другие инструменты
+	getFintoolConvertation(filters, packageData, isQuery) {
+		return this.get(route['getFintoolConvertation'], filters, packageData, isQuery)
+	}
+
+	//Получить расширенный справочник по финансовым инструментам
+	getFintoolReferenceData(filters, packageData, isQuery) {
+		return this.get(route['getFintoolReferenceData'], filters, packageData, isQuery)
+	}
+
+	//Возвращает календарь событий по инструментам за период
+	getCalendarV2(filters, packageData, isQuery) {
+		return this.get(route['getCalendarV2'], filters, packageData, isQuery)
+	}
+
+	//Получить краткий справочник по эмитентам
+	getInfoEmitents(filters, packageData, isQuery) {
+		return this.get(route['getInfoEmitents'], filters, packageData, isQuery)
+	}
+
+	async get(optionsConfig, filters, packageData, isQuery) {
+		if (!packageData && !isQuery) {
+			const data = await this.limiter.schedule(() =>
+				this.getData(optionsConfig, filters)
+			)
+			return utils.xml.obj2xml({ 'item-list': { item: data } })
 		}
 
 		let result = []
-		for (let filter of filters) {
+
+		for (let filter of filters){
 			const tag = Object.keys(filter).filter((k) => k.startsWith('@'))
-			const data = await this.getData(optionsConfig, filter)
-			result.push({ 'item-list': { [tag[0]]: filter[tag[0]], item: data } })
+			try {
+				const data = await this.limiter.schedule(() =>
+					this.getData(optionsConfig, filter)
+				)
+				if (isQuery) {
+					result.push({ batch: { [tag[0]]: filter[tag[0]], 'item-list': { item: data } } })
+				} else {
+					result.push({ 'item-list': { '@tag': filter[tag[0]], '@err': 0, item: data } })
+				}
+			} catch (err) {
+				if (isQuery) {
+					result.push({
+						batch: { [tag[0]]: filter[tag[0]], error: err.toString() }
+					})
+				} else {
+					result.push({ 'item-list': { '@tag': filter[tag[0]], '@err': 1, error: err.toString() } })
+				}
+			}
 		}
 
-		let xml = '<list>'
-		for (let res of result) {
-			xml += utils.obj2xml({ 'item-list': res['item-list'] })
+		let xml = ''
+		if (isQuery) {
+			xml = '<result><batch-list>'
+			for (let batch of result) {
+				xml += utils.xml.obj2xml(batch)
+			}
+			xml += '</batch-list></result>'
+		} else {
+			xml = '<list>'
+			for (let res of result) {
+				xml += utils.xml.obj2xml({ 'item-list': res['item-list'] })
+			}
+			xml += '</list>'
 		}
-		xml += '</list>'
+
 		return xml
 	}
 
@@ -226,33 +427,49 @@ class Api {
 
 		let total
 		const requestOptions = this.parseFilters(filters, optionsConfig.defaults)
+		let url = `${config.rudat.url}/${optionsConfig.url}`
+		if (optionsConfig.prependID){
+			url += filters[optionsConfig.prependID] + optionsConfig.prependUrl
+		}
+		if (optionsConfig.insertID){
+			url += filters[optionsConfig.insertID]
+		}
 		if (optionsConfig.havePages) {
 			total = []
 			let processing = true
+			if(requestOptions.pager) requestOptions.pager.page = 1
+
 			while (processing) {
 				options.body = JSON.stringify(requestOptions)
-				// prettier-ignore
-				const result = await this.request(`${config.rudat.url}/${optionsConfig.url}${optionsConfig.insertID ? filters[optionsConfig.insertID] : ''}`, options)
+				let result = await this.request(url, options)
+				if (!Array.isArray(result)) {
+					if (result.timeTableFields) result = result.timeTableFields
+				}
 				if (result.length === 0) {
-					processing = false
 					break
 				}
 				total = total.concat(result)
-				requestOptions.pageNum++
+
+				//Получили меньше данных, чем минимальное количество на странице - прерываем цикл
+				if (requestOptions.pageSize && requestOptions.pageSize > 0 && result.length < requestOptions.pageSize){
+					break
+				}
+				if (requestOptions.pager) requestOptions.pager.page++
+				if (requestOptions.pageNum) requestOptions.pageNum++
 			}
 		} else {
 			options.body = JSON.stringify(requestOptions)
-			// prettier-ignore
-			total = await this.request(`${config.rudat.url}/${optionsConfig.url}${optionsConfig.insertID ? filters[optionsConfig.insertID] : ''}`, options)
+			total = await this.request(url, options)
 		}
 
 		return total
 	}
 
 	async request(url, options) {
-		const response = await utils.requestUntilSuccess(url, options)
+		const response = await utils.http.requestUntilSuccess(url, options, 60000)
 		if (!response.ok) {
-			throw new Error(`Ошибка! [${response.status}]:${response.statusText}`)
+			const errorText = await response.text()
+			throw new Error(`Ошибка! [${response.status}]:${response.statusText}, ${errorText}`)
 		}
 		return response.json()
 	}
@@ -261,7 +478,7 @@ class Api {
 		const options = {}
 		for (let [key, value] of Object.entries(filters)) {
 			if (Array.isArray(value)) {
-				//[].concat using to create empty array beacuse if there is only one element in array, fast-xml-parser convert it to object inside
+				//[].concat using to create empty array beacuse if there is only one element in array, utils.xml.xml2obj convert it to object inside
 				options[key] = [].concat(value[0][Object.keys(value[0])])
 			} else {
 				options[key] = value
